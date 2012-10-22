c0867de9214dbc642a2c9d16d3fef82b2a2ee8d7
Will S <terribleangel@gmail.com>
2012-10-22 07:57:34 -0400

    Update README to alert users about current error with Mozilla Add-ons page .xpi file.

--- chrome/content/zutilo/include.js
+++ chrome/content/zutilo/include.js
@@ -1,7 +1,3 @@
-var Zotero = Components.classes["@zotero.org/Zotero;1"]
-                       .getService(Components.interfaces.nsISupports)
-                       .wrappedJSObject;
-
 // Only create main object once
 if (!Zotero.Zutilo) {
 	const zutiloLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
