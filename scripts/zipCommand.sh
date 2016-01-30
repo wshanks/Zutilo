e04504668cdc973a0cce72364f9a3d88b164880b
Will S <wsha.code@gmail.com>
2016-01-30 07:05:06 -0500

    Relocate old build scripts

--- scripts/zipCommand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}/../.."
-zip -FSr zutilo.xpi chrome chrome.manifest install.rdf LICENSE.md README.md bootstrap.js -x \*.DS_Store \*.swp
